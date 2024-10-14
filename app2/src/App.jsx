74dccdcea84d3d7939cba2cc630e97cd24d65e75
nikhild006 <nikhildevarajugattu@gmail.com>
2024-10-14 20:13:26 +0530

    Assignment 2

---
+++ app2/src/App.jsx
@@ -0,0 +1,33 @@
+import "./App.css"
+function App(){
+  return(
+    <div>
+      <div className="container">
+      <div className="box">
+        Product 1
+      </div>
+      
+      <div className="box">
+        Product 2
+      </div>
+      
+      <div className="box">
+        Product 3
+      </div>
+      
+      <div className="box">
+        Product 4
+      </div>
+
+      <div className="box">
+        Product 5
+      </div>
+
+      <div className="box">
+        Product 6
+      </div>
+      </div>
+    </div>
+  );
+}
+export default App;
